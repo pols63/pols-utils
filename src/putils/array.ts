e6f5daf99780088f8c419183b48a22ffaf83bc8a
jean.sanchez@grupotawa.com <jean.sanchez@grupotawa.com>
2025-03-19 10:48:52 -0500

    dev

--- src/putils/array.ts
+++ src/putils/array.ts
@@ -86,11 +86,22 @@ export const moveItem = (array: unknown[], originIndex: number, destinationIndex
 export type PLogicalSelector<T> = (Partial<T> & Record<string, unknown>) | ((element: T, index: number) => boolean)
 
 /**
- * Get the first element of an array that pass the logical selector.
+ * Gets the first element of an array that satisfies the given logical selector.
  * @param array Target array.
  * @param logicalSelector Logical selector.
  * @param transform A function that transforms the selected element, if provided.
  * @returns The selected element.
+ * @example
+ * ```javascript
+ * const pets = [
+ *     { type: 'dog', name: 'Fido' },
+ *     { type: 'dog', name: 'Chacalin' },
+ *     { type: 'cat', name: 'Mr. Brown' },
+ *     { type: 'cat', name: 'Night' },
+ * ]
+ * console.log(PUtilsArray.queryOne(pets, pet => pet.type == 'dog')) // Same PUtilsArray.queryOne(pets, { type: 'dog' })
+ * // { type: 'dog', name: 'Fido' },
+ * ```
  */
 export const queryOne = <T, K = T>(array: T[], logicalSelector: PLogicalSelector<T>, transform?: (element: T | null, i: number) => K): K | undefined => {
 	if (array == null || !array.length) return
@@ -136,6 +147,25 @@ export const queryOne = <T, K = T>(array: T[], logicalSelector: PLogicalSelector
  * @param logicalSelector Logical selector.
  * @param transform A function that transforms the selected elements, if provided.
  * @returns A new array with the selected elements.
+ * @example
+ * ```javascript
+ * const pets = [
+ *     { type: 'dog', name: 'Fido' },
+ *     { type: 'dog', name: 'Chacalin' },
+ *     { type: 'cat', name: 'Mr. Brown' },
+ *     { type: 'cat', name: 'Night' },
+ * ]
+ * console.log(PUtilsArray.query(pets, pet => pet.type == 'dog')) // Same PUtilsArray.query(pets, { type: 'dog' })
+ * // [
+ * //     { type: 'dog', name: 'Fido' },
+ * //     { type: 'dog', name: 'Chacalin' },
+ * // ]
+ * console.log(PUtilsArray.query(pets, { type: 'dog' }, pet => pet.name.toUppercase()) // Same PUtilsArray.query(pets, { type: 'dog' })
+ * // [
+ * //     { type: 'dog', name: 'FIDO' },
+ * //     { type: 'dog', name: 'CHACALIN' },
+ * // ]
+ * ```
  */
 export const query = <T, K = T>(array: T[], logicalSelector: PLogicalSelector<T>, transform?: (element: T | null, i: number) => K): K[] => {
 	const results: T[] = []
@@ -252,6 +282,7 @@ export const extract = <T, K = T>(array: T[], logicalSelector: PLogicalSelector<
  * @param setterPropertyName Callback that get the key of each group.
  * @param transform Callback for transform of each element in tha array.
  * @returns A new object with the elements grouped.
+ * @example
  * ```javascript
  * const pets = [
  *     { type: 'dog', name: 'Fido' },
@@ -291,6 +322,30 @@ export const groupBy = <T = never, K = never>(array: K[], setterPropertyName: (e
  * @param array Target array.
  * @param element Element for insert if `check` returns `false` for every element of the array.
  * @param check Callback that check every element in the array.
+ * @example
+ * ```javascript
+ * const pets = [
+ *     { type: 'dog', name: 'Fido' },
+ *     { type: 'dog', name: 'Chacalin' },
+ *     { type: 'cat', name: 'Mr. Brown' },
+ *     { type: 'cat', name: 'Night' },
+ * ]
+ * PUtilsArray.toggleElement(pets, { type: 'dog', name: 'Chacalin' }, pet => pet.name == 'Chacalin')
+ * console.log(pets)
+ * // [
+ * //     { type: 'dog', name: 'Fido' },
+ * //     { type: 'cat', name: 'Mr. Brown' },
+ * //     { type: 'cat', name: 'Night' },
+ * // ]
+ * PUtilsArray.toggleElement(pets, { type: 'dog', name: 'Chacalin' }, pet => pet.name == 'Chacalin')
+ * console.log(pets)
+ * // [
+ * //     { type: 'dog', name: 'Fido' },
+ * //     { type: 'cat', name: 'Mr. Brown' },
+ * //     { type: 'cat', name: 'Night' },
+ * //     { type: 'dog', name: 'Chacalin' },
+ * // ]
+ * ```
  */
 export const toggleElement = <T>(array: T[], element: T, check: (element: T, index?: number) => boolean) => {
 	let index: number | null = null
@@ -314,6 +369,17 @@ export const toggleElement = <T>(array: T[], element: T, check: (element: T, ind
  * @param array Target array.
  * @param length Max length of the chunk.
  * @returns A chunks array.
+ * @example
+ * ```javascript
+ * const mynumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+ * console.log(PUtilsArray.chunks(mynumbers, 3))
+ * // [
+ * //     [0, 1, 2],
+ * //     [3, 4, 5],
+ * //     [6, 7, 8],
+ * //     [9],
+ * // ]
+ * ```
  */
 export const chunks = <T>(array: T[], length: number): T[][] => {
 	const chunks: T[][] = []
@@ -331,6 +397,20 @@ export const chunks = <T>(array: T[], length: number): T[][] => {
  * @param setterPropertyName Function that returns the key of the index.
  * @param formatElement Transformation elements.
  * @returns An object with the indexed elements of the original array.
+ * @example
+ * ```javascript
+ * const pets = [
+ *     { type: 'dog', name: 'Fido' },
+ *     { type: 'dog', name: 'Chacalin' },
+ *     { type: 'cat', name: 'Mr. Brown' },
+ *     { type: 'cat', name: 'Night' },
+ * ]
+ * console.log(PUtilsArray.index(pets, pet => pet.type))
+ * // {
+ * //     dog: { type: 'dog', name: 'Fido' },
+ * //     cat: { type: 'cat', name: 'Mr. Brown' },
+ * // }
+ * ```
  */
 export const index = <T, K = never>(array: T[], setterPropertyName: (element: T, index?: number) => string, formatElement?: (element: T, index?: number) => K) => {
 	const result: Record<string, [K] extends [never] ? T : K> = {}
@@ -344,5 +424,9 @@ export const index = <T, K = never>(array: T[], setterPropertyName: (element: T,
  * Get a new array with distinct elements. Only compare elements of primitive types.
  * @param array Target array.
  * @returns A new array with distinct elements.
+ * @example
+ * ```javascript
+ * const mynumbers = [0, 1, 2, 2, 2, 5, 6, 7, 7, 7]
+ * console.log(PUtilsArray.distinct(mynumbers)) // [0, 1, 2, 5, 6, 7]
  */
 export const distinct = <T>(array: T[]) => Array.from(new Set(array))
--- src/putils/arrayBuffer.ts
+++ src/putils/arrayBuffer.ts
@@ -1,3 +1,9 @@
 type EncodingType = "utf-8" | "utf-16le" | "utf-16be" | "iso-8859-1" | "windows-1252"
 
+/**
+ * Transform a array buffer to string.
+ * @param data Array buffer data.
+ * @param encode Encode type: `"utf-8" | "utf-16le" | "utf-16be" | "iso-8859-1" | "windows-1252"`. `utf-8` by default.
+ * @returns The converted string.
+ */
 export const toString = (data: ArrayBuffer, encode: EncodingType = 'utf-8') => new TextDecoder(encode).decode(new Uint8Array(data))
